008a86e4026b5a378085352377cd6650d588d45e
jorenham <jhammudoglu@gmail.com>
2025-07-25 01:51:13 +0200

    remove builds that are identical to the latest pypi releases

--- build.py
+++ build.py
@@ -1,15 +1,20 @@
 # /// script
-# requires-python = ">=3.12"
-# dependencies = ["uv", "jinja2"]
+# requires-python = ">=3.13"
+# dependencies = ["jinja2>=3.1.6", "uv>=0.8.3"]
 # ///
 
+# pyright: reportAny=false
+
 import datetime as dt
+import hashlib
+import json
 import re
 import shutil
 import subprocess
 import sys
+import urllib.request
 from pathlib import Path
-from typing import Final, NamedTuple, final, override
+from typing import Any, Final, NamedTuple, final, override
 
 import jinja2
 
@@ -43,6 +48,28 @@ def _get_template(fname: str) -> jinja2.Template:
     )
 
 
+def _sha256sum(path: str | Path, /) -> str:
+    with Path(path).open("rb") as fp:
+        digest = hashlib.file_digest(fp, "sha256")
+    return digest.hexdigest()
+
+
+def _fetch_json(
+    url: str,
+    /,
+    *,
+    headers: dict[str, str] | None = None,
+    timeout: float = 5,
+) -> dict[str, Any]:  # pyright: ignore[reportExplicitAny]
+    """Make a JSON request to the given URL."""
+    request = urllib.request.Request(url, headers=headers or {})
+    with urllib.request.urlopen(request, timeout=timeout) as response:
+        if response.status != 200:
+            raise IOError(f"Failed to fetch {url}: {response.status} {response.reason}")
+        contents = response.read()
+    return json.loads(contents)
+
+
 class Version(NamedTuple):
     """A version tuple with a custom string representation."""
 
@@ -62,6 +89,11 @@ class Version(NamedTuple):
 type VersionRange = tuple[Version, Version]
 
 
+class DistInfo[T](NamedTuple):
+    sdist: T
+    wheel: T
+
+
 @final
 class Project:
     np_range: Final[VersionRange]
@@ -76,20 +108,26 @@ class Project:
         return f"{self.np_range[0]}.{BUILD}"
 
     @property
-    def distname(self, /) -> str:
+    def name(self, /) -> str:
         return f"{NAME}-{self.version}"
 
     @property
-    def path_project(self, /) -> Path:
-        return DIR_PROJECTS / self.distname
+    def project_path(self, /) -> Path:
+        return DIR_PROJECTS / self.name
 
     @property
-    def path_wheel(self, /) -> Path:
-        return DIR_DIST / f"{self.distname}-py3-none-any.whl"
+    def dist_paths(self, /) -> DistInfo[Path]:
+        return DistInfo(
+            sdist=DIR_DIST / f"{self.name}.tar.gz",
+            wheel=DIR_DIST / f"{self.name}-py3-none-any.whl",
+        )
 
     @property
-    def path_sdist(self, /) -> Path:
-        return DIR_DIST / f"{self.distname}.tar.gz"
+    def dist_hashes(self, /) -> DistInfo[str]:
+        return DistInfo(
+            sdist=_sha256sum(self.dist_paths.sdist),
+            wheel=_sha256sum(self.dist_paths.wheel),
+        )
 
     @property
     def const_name(self, /) -> str:
@@ -97,15 +135,12 @@ class Project:
 
     @override
     def __str__(self, /) -> str:
-        return self.distname
+        return self.name
 
     @override
     def __repr__(self, /) -> str:
         clsname = type(self).__name__
         return f"<{clsname} np_range={self.np_range} py_range={self.py_range}>"
-
-    def __fspath__(self, /) -> str:
-        return str(self.path_project)
 
     def _render_to_string(self, fname: str, /) -> str:
         np_start, np_stop = self.np_range
@@ -119,7 +154,7 @@ class Project:
         return _get_template(fname).render(**context)
 
     def _create_project(self, /):
-        project_dir = self.path_project
+        project_dir = self.project_path
         project_dir.mkdir(parents=True, exist_ok=True)
 
         for fname in ("LICENSE", "README.md"):
@@ -142,21 +177,23 @@ class Project:
         _ = py_typed_path.write_text("")
 
     def build(self, /) -> None:
+        """Create and `uv build` the projects."""
         self._create_project()
 
         cmd = [
             "uv",
             "build",
-            f"--directory=./{self.path_project.relative_to(Path.cwd())}",
+            f"--directory=./{self.project_path.relative_to(Path.cwd())}",
             f"--out-dir={DIR_DIST}",
         ]
-        print("$", " ".join(cmd))
+        print("$", " ".join(cmd), file=sys.stderr)
 
         completed = subprocess.run(cmd, capture_output=True, text=True)
         _ = sys.stderr.write(completed.stderr)
         _ = sys.stdout.write(completed.stdout)
         completed.check_returncode()
 
+        # verify that the build was successful
         paths: list[Path] = []
         for match in re.finditer(r"Successfully built (/[\w\-\./]+)", completed.stderr):
             out_path = Path.cwd() / match.group(1)
@@ -175,14 +212,9 @@ class Project:
         if path_wheel.suffix != ".whl":
             path_wheel, path_sdist = path_sdist, path_wheel
 
-        assert path_wheel == self.path_wheel, (path_wheel, self.path_wheel)
-        assert path_sdist == self.path_sdist, (path_sdist, self.path_sdist)
-
-    @staticmethod
-    def clean_generated() -> None:
-        """Clean all projects sources."""
-        if DIR_PROJECTS.exists():
-            shutil.rmtree(str(DIR_PROJECTS))
+        paths_expect = self.dist_paths
+        assert path_sdist == paths_expect.sdist, (path_sdist, paths_expect.sdist)
+        assert path_wheel == paths_expect.wheel, (path_wheel, paths_expect.wheel)
 
 
 PROJECTS = [
@@ -213,16 +245,79 @@ PROJECTS = [
 ]
 
 
-def main() -> int:
-    Project.clean_generated()
+def _fetch_latest_release_hashes() -> DistInfo[dict[Version, tuple[int, str]]]:
+    # https://peps.python.org/pep-0691/
+    # https://docs.pypi.org/api/index-api/#json_1
+    data = _fetch_json(
+        f"https://files.pythonhosted.org/simple/{NAME}",
+        headers={
+            "Host": "pypi.org",
+            "Accept": "application/vnd.pypi.simple.v1+json",
+        },
+    )
+    if "files" not in data:
+        raise ValueError(f"Invalid response from PyPI: {data!r}")
+
+    # keep only the latest sdist and wheel files for each numpy version
+    # {Version: (build, sha256)}
+    latest_sdists: dict[Version, tuple[int, str]] = {}
+    latest_wheels: dict[Version, tuple[int, str]] = {}
+    for file in data["files"]:
+        fname: str = file["filename"]
+        if fname.endswith(".whl"):
+            # e.g. numpy_typing_compat-1.22.20250724-py3-none-any.whl
+            pattern = rf"{NAME}-(\d)\.(\d+)\.(\d+)-py3-none-any\.whl"
+            target = latest_wheels
+        else:
+            # e.g. numpy_typing_compat-1.25.20250724.tar.gz
+            pattern = rf"{NAME}-(\d)\.(\d+)\.(\d+)\.tar\.gz"
+            target = latest_sdists
+
+        match = re.match(pattern, fname)
+        assert match, file
+
+        np_version = Version(int(match.group(1)), int(match.group(2)))
+        build = int(match.group(3))
+        assert build > 2025_06_00, file
+
+        if np_version not in target or target[np_version][0] < build:
+            target[np_version] = build, file["hashes"]["sha256"]
+
+    return DistInfo(sdist=latest_sdists, wheel=latest_wheels)
+
+
+def main(*args: str) -> int:
+    # [version]: (sha256_sdist, sha256_wheel)
+    latest_hashes = _fetch_latest_release_hashes()
 
     for project in PROJECTS:
         project.build()
+        np_version = project.np_range[0]
+        hashes = project.dist_hashes
+        paths = project.dist_paths
 
-    # Project.clean_generated()
+        for build_hashes, hash_, path in zip(latest_hashes, hashes, paths):
+            pypi_build, pypi_hash = build_hashes[np_version]
+
+            if pypi_hash == hash_:
+                print(
+                    f"no changes since {np_version}.{pypi_build} - removing {path}",
+                    file=sys.stderr,
+                )
+                path.unlink()
+            else:
+                # only print the paths of new builds to stdout
+                print(path)
+
+    # TODO: remove sdist/wheel if their hashes match the latest existing PyPI
+    # version of the numpy branch
+
+    if "--keep" not in args:
+        if DIR_PROJECTS.exists():
+            shutil.rmtree(str(DIR_PROJECTS))
 
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(*sys.argv[1:]))
